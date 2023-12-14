fd95d6e769fed117bdc1464d797076f46bad8bd3
peter kim <peterkim1025@gmail.com>
2023-12-13 22:29:17 -0500

    more update after break

--- models/index.js
+++ models/index.js
@@ -18,7 +18,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false
   },
-  as: 'product_tag'
+  as: 'saved_tags'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,{
@@ -26,7 +26,7 @@ Tag.belongsToMany(Product,{
     model: ProductTag,
     unique: false
   },
-  as: "tag_product"
+  as: "tag_products"
 })
 module.exports = {
   Product,
